6a372ec9160bb79a11eb207e75a7cb88538d1e76
AnkitaShanbhag30 <ankita.sshanbhag@gmail.com>
2024-04-24 23:17:28 -0700

    Search Charaters and Resonated fields done

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,31 @@ class MyApp extends StatelessWidget {
       title: 'Reelatable',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Color(0xFF070D35),
           background: Color(0xFF070D35),
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Reelatable'),
+      home: const MainScreen(),  // Changed from MyHomePage to MainScreen
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Welcome to Reelatable')),
+      body: Column(
+        children: const [
+          Expanded(
+            child: MyHomePage(title: 'Reelatable'),  // Using MyHomePage as a child widget
+          ),
+          // You can add more widgets here as needed
+        ],
+      ),
     );
   }
 }
@@ -36,77 +55,182 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _textController = TextEditingController();
-  String _responseText = '';
+  List<Map<String, dynamic>> movies = [];
+  Map<String, dynamic> selectedMovie = {};
+  bool showResonated = false; // State to toggle display of resonated data
+  String _errorMessage = '';
+  int _selectedTabIndex = 0; // To track the selected tab
+
+   // Change to store detailed information
+  Map<String, Map<String, String>> userResonatedData = {};
 
   Future<void> _sendDataToBackend([String? movieName]) async {
     movieName ??= _textController.text;
-    var url = Uri.parse('http://flask-reelatable-service.default.svc.cluster.local:5000/api');
+    var url = Uri.parse('http://127.0.0.1:5001/metadata/get_movie_metadata?title=' + Uri.encodeComponent(movieName));
     try {
-      var response = await http.post(
-        url,
-        headers: {"Content-Type": "application/json"},
-        body: json.encode({'movieName': movieName}),
-      );
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
-
-      // Set the state to update the response text on UI
-      setState(() {
-        _responseText = json.decode(response.body)['message'] ?? 'No response';
-      });
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        setState(() {
+          movies.add(data);
+          _errorMessage = '';
+        });
+      } else {
+        setState(() {
+          _errorMessage = 'Failed to fetch data for the movie: $movieName';
+        });
+      }
     } catch (e) {
-      print('Error sending data to backend: $e');
       setState(() {
-        _responseText = 'Error sending data to backend: $e';
+        _errorMessage = 'Error sending data to backend: $e';
       });
     }
   }
 
-  @override
+Widget attributeList(Map<String, dynamic> movie, String attributeKey) {
+    List<Widget> listItems = [];
+    for (int i = 1; i <= 5; i++) {
+      var traitKey = '${attributeKey}_${i}_trait';
+      var evidenceKey = '${attributeKey}_${i}_evidence';
+      if (movie.containsKey(traitKey) && movie.containsKey(evidenceKey)) {
+        String fullKey = '${movie['title']} - ${movie[traitKey]}'; // Combining movie title and trait
+
+        listItems.add(CheckboxListTile(
+          title: Text(movie[traitKey], style: TextStyle(color: Color(0xFFF2DBAF))),
+          subtitle: Text(movie[evidenceKey], style: TextStyle(color: Color(0xFFF2DBAF))),
+          value: userResonatedData.containsKey(fullKey),
+          onChanged: (bool? value) {
+            setState(() {
+              if (value == true) {
+                userResonatedData[fullKey] = {
+                  'trait': movie[traitKey],
+                  'evidence': movie[evidenceKey],
+                  'movie': movie['title']
+                };
+              } else {
+                userResonatedData.remove(fullKey);
+              }
+            });
+          },
+          activeColor: Colors.red,
+        ));
+      }
+    }
+    return SingleChildScrollView(
+      child: Column(children: listItems),
+    );
+  }
+
+  Widget buildResonatedList() {
+    List<Widget> items = userResonatedData.entries.map((entry) => ListTile(
+      title: Text("${entry.value['trait']} (${entry.value['movie']})", style: TextStyle(color: Color(0xFFF2DBAF))),
+      subtitle: Text(entry.value['evidence']!, style: const TextStyle(color: Color(0xFFF2DBAF)))
+    )).toList();
+
+    return SingleChildScrollView(
+      child: Column(children: items),
+    );
+  }
+
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
-        backgroundColor: Color(0xFF773A7E),
+        backgroundColor: Color(0xFF070D35),
         foregroundColor: Color(0xFFF2DBAF),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 300),
-        color: Color(0xFF070D35),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Enter a movie that you like',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 10),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 300),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_errorMessage, style: TextStyle(color: Colors.red, fontSize: 16)),
+              ),
             TextField(
               controller: _textController,
-              onSubmitted: _sendDataToBackend,
               decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: Color(0xFF070D35)),
                 fillColor: Color(0xFFF2DBAF),
                 filled: true,
                 border: OutlineInputBorder(),
-                labelText: 'Search',
+                labelText: 'List movies that made the deepest impact on you',
               ),
+              onSubmitted: _sendDataToBackend,
             ),
-            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => _sendDataToBackend(),
               child: const Text('Add'),
             ),
-            const SizedBox(height: 20),
-            Text(
-              _responseText,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
+            Wrap(
+              spacing: 20,
+              children: movies.map((movie) => InkWell(
+                onTap: () => setState(() => selectedMovie = movie),
+                child: Image.network(movie['poster_url'], width: 100, height: 150, fit: BoxFit.cover),
+              )).toList(),
             ),
+            if (selectedMovie.isNotEmpty) ...[
+              Text(selectedMovie['protagonist'] ?? 'Protagonist not found', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFFF2DBAF))), // Color updated here
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text("Select characteristics that resonate with you", style: TextStyle(fontSize: 18, color: Color(0xFFF2DBAF))), // Color updated here
+              ),
+              DefaultTabController(
+                length: 4,
+                child: Column(
+                  children: <Widget>[
+                    TabBar(
+                      onTap: (index) {
+                        setState(() {
+                          _selectedTabIndex = index;
+                        });
+                      },
+                      indicatorColor: Colors.red,
+                      labelColor: Colors.red,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(text: 'Flaws'),
+                        Tab(text: 'personality_traits'),
+                        Tab(text: 'Desires'),
+                        Tab(text: 'Beliefs'),
+                      ],
+                    ),
+                    Container(
+                      height: 300,
+                      child: TabBarView(
+                        children: [
+                          attributeList(selectedMovie, 'flaws'),
+                          attributeList(selectedMovie, 'personality_traits'),
+                          attributeList(selectedMovie, 'desires'),
+                          attributeList(selectedMovie, 'beliefs'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showResonated = !showResonated; // Toggle the display of resonated items
+                  });
+                },
+                child: Text('Show Resonated'),
+              ),
+              if (showResonated) buildResonatedList(),
+            ]
           ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 
   @override
   void dispose() {
@@ -114,3 +238,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 }
+
+
+
+
+
